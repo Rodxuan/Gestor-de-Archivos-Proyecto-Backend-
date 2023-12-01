3f9ca9b7bb0b66f499a3d6c0b408db258add94c5
Rodxuan <loesskd@gmail.com>
2023-12-01 14:47:37 +0100

    El código define un esquema de Mongoose para un usuario y se exporta como el modelo "Usuario".

---
+++ models/usuario.js
@@ -0,0 +1,29 @@
+const {Schema, model} = require('mongoose')
+
+const UsuarioSchema = Schema({
+
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        default:'WORKER'
+    }
+})
+
+UsuarioSchema.method('toJSON', function(){
+  const {__v, password,  ...object} = this.toObject()
+  return object
+})
+
+module.exports = model('Usuario', UsuarioSchema)
